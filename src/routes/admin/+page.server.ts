eb3ea92964b85a043bb462ff3500aa4ee5d523de
kristijan-rotim <pushes.02.waifs@icloud.com>
2025-02-21 10:41:25 +0100

    WIP

--- src/routes/admin/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export async function load({ locals, url }) {
-	if (!locals.user) {
-		throw redirect(303, `/login?redirectTo=${encodeURIComponent(url.pathname)}`);
-	}
-	return { user: locals.user };
-}

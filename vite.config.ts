9bf7654fb4aab5fe726c4d22f283376052a0d39d
kristijan-rotim <pushes.02.waifs@icloud.com>
2025-02-23 15:58:52 +0100

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['rajsavac.space'], // Add your domain here
+		allowedHosts: ['https://rajsavac.space'], // Add your domain here
 		host: '0.0.0.0', // Ensure the server binds to all interfaces
 		port: 5173
 	}
